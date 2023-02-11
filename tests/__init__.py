38b9d310a6248ced5a4bf327dcc1479e82da774d
Ong Tze Wei <tzewei.ong@mediamonks.com>
2023-02-11 15:59:00 +0800

    Incorporate use of pre-commit and github actions

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 
-class AppTestCase(TestCase):
-  def setUp(self) -> None:
-    print('Setting up')
 
-  def test_sample(self):
-    print('Running sample test case')
+class AppTestCase(TestCase):
+    def setUp(self) -> None:
+        print('Setting up')
+
+    def test_sample(self):
+        print('Running sample test case')
