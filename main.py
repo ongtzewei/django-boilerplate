fc3dfc38792bd7e5d00b2e02efdb9f89e8e5262f
Tze Wei <ong.tzewei@gmail.com>
2026-02-07 12:01:18 +0800

    [Tze Wei] Bugfix for ruff and separating unit test from sonarqube jobs in github action

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_boilerplate.settings')
@@ -9,11 +10,11 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and available "
-            "on your PYTHONPATH environment variable? Did you forget to activate a "
-            "virtual environment?"
+            'on your PYTHONPATH environment variable? Did you forget to activate a '
+            'virtual environment?'
         ) from exc
     execute_from_command_line(sys.argv)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
